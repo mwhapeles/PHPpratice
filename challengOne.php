b365ddc7c04b7c2f53cab2e7f9a3134a07ae6b95
mwhapeles <114778814+mwhapeles@users.noreply.github.com>
2024-05-25 19:46:55 -0700

    challenge

---
+++ challengOne.php
@@ -0,0 +1,14 @@
+<?php 
+$name = "Teresa"; //Heading one
+$city = "NYC"; 
+$movie = "Star Wars"; //Italics
+$friend = "George"; //Bold
+$candy = "Sour Patch Kids"; 
+?>
+
+<h1><?php echo $name; ?></h1>
+
+<?php echo "<p>Lives in $city. Last week she went with her friend, <b>$friend</b>, to see <i>$movie</i></p>\n\n";
+
+echo "<p>Unfortunately, the ate so many $candy, they had to leave the movie early!</p> \n\n;"; ?>
+
